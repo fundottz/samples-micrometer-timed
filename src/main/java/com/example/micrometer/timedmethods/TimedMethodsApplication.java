38be79d50ecb0e5edfaa9cde18f37b3093364fca
Andrey Dotts <dottsaa@msk.bcs.ru>
2019-11-06 12:25:56 +0300

    Add timed on component

--- src/main/java/com/example/micrometer/timedmethods/TimedMethodsApplication.java
+++ src/main/java/com/example/micrometer/timedmethods/TimedMethodsApplication.java
@@ -3,6 +3,7 @@ package com.example.micrometer.timedmethods;
 import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,18 +12,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class TimedMethodsApplication {
+
+  @Autowired
+  private Worker worker;
 
   public static void main(String[] args) {
     SpringApplication.run(TimedMethodsApplication.class, args);
   }
 
-  @Timed("my_metric")
   @Scheduled(fixedRate = 1000)
-  public void doJob() {
-    System.out.println("work");
+  public void scheduleJob() {
+    worker.doJob();
   }
 
   @Configuration
@@ -38,6 +42,15 @@ public class TimedMethodsApplication {
     @Bean
     public TimedAspect timedAspect(MeterRegistry registry) {
       return new TimedAspect(registry);
+    }
+  }
+
+  @Component
+  public class Worker {
+
+    @Timed("my_metric")
+    public void doJob() {
+      System.out.println("work");
     }
   }
 }
